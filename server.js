94b8d23d8935edd69112fea69cca5385da44f203
zhyrax79-oss <zhyrax79@gmail.com>
2026-06-16 09:25:10 +0300

    fix render

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const session = require("express-session");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
