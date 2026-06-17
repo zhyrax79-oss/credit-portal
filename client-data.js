5d0cb9983db7164977ee33b7a6cdda048c356486
zhyrax79-oss <zhyrax79@gmail.com>
2026-06-17 11:21:28 +0300

    Add files via upload

---
+++ client-data.js
@@ -0,0 +1,19 @@
+const client = {
+    fio: "Иванов Иван Иванович",
+    rating: "812",
+    activeCredits: "2",
+    limit: "1 050 000 ₽",
+    approval: "92%",
+    status: "Физическое лицо",
+    security: "Активна",
+    lastLogin: "Сегодня 09:42",
+
+    declarationAccount: "1 250 000 ₽",
+    repaymentAccount: "325 000 ₽",
+
+    credit1: "Потребительский кредит №458721",
+    credit1Balance: "18 500 ₽",
+
+    credit2: "Автокредит №784521",
+    credit2Balance: "67 000 ₽"
+};
